07298f9acc3c14b1a5326366ee0c870184e83aec
Никита Мигунов <kagestonedragon@gmail.com>
2020-04-12 04:27:10 +0300

    Начала верстки

---
+++ markups/vendor/dist/0.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./assets/scss/webpack.scss":
+/*!**********************************!*\
+  !*** ./assets/scss/webpack.scss ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./assets/scss/webpack.scss?");
+
+/***/ })
+
+}]);
