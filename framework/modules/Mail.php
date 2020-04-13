6ac2950a23ef028939854fcba86e03658c954da4
Никита Мигунов <kagestonedragon@gmail.com>
2020-04-14 02:57:58 +0300

    test PhpMailer

--- framework/modules/Mail.php
+++ framework/modules/Mail.php
@@ -23,7 +23,7 @@ class Mail
         $mail->SMTPSecure = 'tls';   // Enable encryption, 'ssl'
         $mail->From = 'sender@domain.com'; // The FROM field, the address sending the email
         $mail->FromName = 'Orlie'; // The NAME field which will be displayed on arrival by the email client
-        $mail->addAddress('recipient@domain.net', 'BOB');     // Recipient's email address and optionally a name to identify him
+        $mail->addAddress('kagestonedragon@gmail.com', 'BOB');     // Recipient's email address and optionally a name to identify him
         $mail->isHTML(true);   // Set email to be sent as HTML, if you are planning on sending plain text email just set it to false
 // The following is self explanatory
         $mail->Subject = 'Email sent with Mailgun';
