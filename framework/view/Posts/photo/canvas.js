fd747fc55b27525a1364d2e159bd0a6639915b28
Kage Stonedragon <kagestonedragon@gmail.com>
2020-05-04 01:37:14 +0300

    ППравки редактора изображений

--- framework/view/Posts/photo/canvas.js
+++ framework/view/Posts/photo/canvas.js
@@ -1,15 +1,7 @@
-function Canvas(DOMId, width, height) {
-    this.canvas = document.getElementById(DOMId);
-    this.context = this.canvas.getContext('2d');
-    this.width = width;
-    this.height = height;
-}
 
-Canvas.prototype.clear = function() {
-    this.width = this.width;
-}
 
-Canvas.prototype.update = function() {
+/*Canvas.prototype.update = function() {
+    console.log(this);
     requestAnimationFrame(this.update.bind(this));
     this.clear();
-}
+}*/
--- framework/view/Posts/photo/view.php
+++ framework/view/Posts/photo/view.php
@@ -8,12 +8,61 @@ Application::attachJsScript(__DIR__ . '/canvas.js');
 <canvas id="canvas" style="border: 3px solid blue;"></canvas>
 
 <script>
-    function Picture() {
-        this.offsetX = 0;
-        this.offsetY = 0;
-        this.scale = 1;
+    function getMouse(element) {
+        const mouse = {
+            x: 0,
+            y: 0,
+            dx: 0,
+            dy: 0,
+            left: false,
+            wheel: 0
+        };
+
+        mouse.update = function() {
+            mouse.dx = 0;
+            mouse.dy = 0;
+            mouse.wheel = 0;
+        };
+
+        element.addEventListener('mousemove', function(e) {
+            const rect = element.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+            mouse.dx = x - mouse.x;
+            mouse.dy = y - mouse.y;
+            mouse.x = x;
+            mouse.y = y;
+        });
+        element.addEventListener('mousedown', function(e) {
+            if (e.button === 0) {
+                mouse.left = true;
+            }
+        });
+        element.addEventListener('mouseup', function(e) {
+            if (e.button === 0) {
+                mouse.left = false;
+            }
+        });
+        element.addEventListener('mousewheel', function(e) {
+            e.preventDefault();
+            mouse.wheel = e.deltaY;
+        });
     }
 
-    const canvas = new Canvas('canvas', 750, 750);
-    canvas.update();
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+    canvas.width = 750;
+    canvas.height = 750;
+
+    update();
+    function update() {
+        requestAnimationFrame(update);
+        clearCanvas();
+    }
+
+    function clearCanvas() {
+        canvas.width = canvas.width;
+    }
+
+
 </script>
